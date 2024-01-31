9f5288b7b662dddfb7fd245c12f469d17882bcfa
Gaetano <gaem195@gmail.com>
2024-01-31 16:48:44 +0100

    Add third exercise

--- eserS1L3C/Program.cs
+++ eserS1L3C/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Dimmi quanti numeri vuoi inserire: ");
+            int totalNumbers = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[totalNumbers];
+
+            int sum = 0;
+            double media;
+            for (int i = 0; i < totalNumbers; i++)
+            {
+                Console.Write($"Inserisci il {i + 1}° numero: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+
+                sum += numbers[i];
+            }
+
+            media = (double)sum / (double)totalNumbers;
+
+            Console.WriteLine($"La somma di tutti i tuoi numeri è: {sum}");
+            Console.WriteLine($"La media di tutti i numeri è: {media}");
+
+
+            Console.ReadKey();
         }
     }
 }
